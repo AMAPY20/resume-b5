b76b73805422c0f7e6f1d4e3b6b14ae87bce3f80
amapy20 <engineer.ama.2021@gmail.com>
2021-07-03 19:41:42 +0300

    V.1

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+var typed = new Typed('.animate', {
+    strings: [
+        "UX",
+        "UX",
+    ],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop:true,
+});
+
+
+
+const sr = ScrollReveal({
+    distance: "60px",
+    duration: 2800,
+    // reset: true,
+});
+
+sr.reveal(`.profile-text, .contact-sr`, {
+  origin: "top",
+  delay: 200,
+});
+
+sr.reveal(`.profile, .experience`, {
+  origin: "top",
+  interval: 100,
+  delay: 1000,
+});
+sr.reveal(`.reference`, {
+  origin: "top",
+  interval: 300,
+//   delay: 1000,
+});
+sr.reveal(`.education,.title`, {
+  origin: "buttom",
+  interval: 300,
+  //   delay: 1000,
+});
